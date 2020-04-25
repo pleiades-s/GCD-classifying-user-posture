f70b5c68267d580cc40b1064c016cfde1c37cd44
Yun <rudobco0825@gmail.com>
2020-04-25 19:43:12 +0900

    한손으로 들고 있는지 아닌지 구분하도록 함

---
+++ app/src/main/java/com/example/gcd_testapp/GyroSensor.java
@@ -0,0 +1,4 @@
+package com.example.gcd_testapp;
+
+public class GyroSensor {
+}
--- app/src/main/java/com/example/gcd_testapp/MainActivity.java
+++ app/src/main/java/com/example/gcd_testapp/MainActivity.java
@@ -1,36 +1,104 @@
 package com.example.gcd_testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.ListView;
 import android.app.PendingIntent;
 import android.preference.PreferenceManager;
 import android.content.SharedPreferences;
 import android.view.View;
+import android.widget.Toast;
+
 import com.google.android.gms.location.ActivityRecognitionClient;
 import com.google.android.gms.location.DetectedActivity;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity
-        implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     private Context mContext;
     public static final String DETECTED_ACTIVITY = ".DETECTED_ACTIVITY";
-//Define an ActivityRecognitionClient//
+    //Define an ActivityRecognitionClient//
 
     private ActivityRecognitionClient mActivityRecognitionClient;
     private ActivitiesAdapter mAdapter;
+
+    // Gyro sensor
+    private SensorManager sensorManager;
+    private Sensor gyroscopeSensor;
+    private  SensorEventListener gyroscopeEventListener;
+
+    // Accelerometor sensor
+    private Sensor accelorSensor;
+    private SensorEventListener accelorEventListener;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        accelorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+        if (gyroscopeSensor == null){
+            Toast.makeText(this, "The device has no Gyroscope", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        if (accelorSensor == null){
+            Toast.makeText(this, "The device has no Accelerometer", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        gyroscopeEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+
+                String log = String.valueOf(sensorEvent.values[0]) + " " + String.valueOf(sensorEvent.values[1])  + " " + String.valueOf(sensorEvent.values[2]);
+                Log.d("gyro", log);
+                if (sensorEvent.values[0] < 0.005f && sensorEvent.values[0] > -0.005f){
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                }
+                else{
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        accelorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+
+                String log = String.valueOf(sensorEvent.values[0]) + " " + String.valueOf(sensorEvent.values[1])  + " " + String.valueOf(sensorEvent.values[2]);
+                Log.d("acceler", log);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
         mContext = this;
 
-//Retrieve the ListView where we’ll display our activity data//
+        //Retrieve the ListView where we’ll display our activity data//
         ListView detectedActivitiesListView = (ListView) findViewById(R.id.activities_listview);
 
         ArrayList<DetectedActivity> detectedActivities = ActivityIntentService.detectedActivitiesFromJson(
@@ -48,15 +116,21 @@ public class MainActivity extends AppCompatActivity
         PreferenceManager.getDefaultSharedPreferences(this)
                 .registerOnSharedPreferenceChangeListener(this);
         updateDetectedActivitiesList();
+
+        sensorManager.registerListener(gyroscopeEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(accelorEventListener, accelorSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
     @Override
     protected void onPause() {
         PreferenceManager.getDefaultSharedPreferences(this)
                 .unregisterOnSharedPreferenceChangeListener(this);
         super.onPause();
+
+        sensorManager.unregisterListener(gyroscopeEventListener);
+        sensorManager.unregisterListener(accelorEventListener);
     }
     public void requestUpdatesHandler(View view) {
-//Set the activity detection interval. I’m using 3 seconds//
+    //Set the activity detection interval. I’m using 3 seconds//
         Task<Void> task = mActivityRecognitionClient.requestActivityUpdates(
                 3000,
                 getActivityDetectionPendingIntent());
